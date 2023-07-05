6451cfac95b5d0c8d38d9bf303e35ffebbcc122f
Hayden Bleasel <hello@haydenbleasel.com>
2023-07-05 00:42:17 -0400

    Update tailwind.config.ts

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -8,7 +8,45 @@ const config: Config = {
     './components/**/*.{ts,tsx}',
     './node_modules/@beskar-labs/gravity/**/*.js',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          pre: {
+            margin: 0,
+            backgroundColor: theme('colors.black'),
+            borderRadius: '11px',
+            display: 'grid',
+            '& > code': {
+              display: 'grid',
+              minWidth: '100%',
+              overflowWrap: 'break-word',
+              padding: 0,
+            },
+          },
+          code: {
+            backgroundColor: theme('colors.neutral.900'),
+            borderRadius: theme('borderRadius.md'),
+            paddingLeft: theme('padding.2'),
+            paddingRight: theme('padding.2'),
+            paddingTop: theme('padding.1'),
+            paddingBottom: theme('padding.1'),
+            counterReset: 'line',
+            fontWeight: theme('fontWeight.normal'),
+            '&::before': {
+              display: 'none',
+            },
+            '&::after': {
+              display: 'none',
+            },
+            '& > span': {
+              display: 'inline-block',
+            },
+          },
+        },
+      }),
+    },
+  },
   plugins: [typography],
 };
 
