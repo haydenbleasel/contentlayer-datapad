d9594047795b8a512c9532700203c6acf7ec6fd3
Hayden Bleasel <hello@haydenbleasel.com>
2023-07-04 23:58:34 -0400

    Install contentlayer

---
+++ website/contentlayer.config.ts
@@ -0,0 +1,38 @@
+import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+import { computeFields, remarkPlugins, rehypePlugins } from '../';
+
+export const Blog = defineDocumentType(() => ({
+  name: 'Blog',
+  filePathPattern: `blog/**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+    description: {
+      type: 'string',
+      required: true,
+    },
+    date: {
+      type: 'date',
+      required: true,
+    },
+    image: {
+      type: 'string',
+      required: false,
+    },
+  },
+  computedFields: computeFields<'Blog'>({}),
+}));
+
+const source = makeSource({
+  contentDirPath: './content',
+  documentTypes: [Blog],
+  mdx: {
+    remarkPlugins,
+    rehypePlugins,
+  },
+});
+
+export default source;
